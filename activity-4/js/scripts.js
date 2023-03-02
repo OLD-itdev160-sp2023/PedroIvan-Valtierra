4c37c417c4fe698287c3f66b287e16c4e56e4d07
valtiep1 <valtiep1@gmatc.matc.edu>
2023-03-02 14:21:13 -0600

    Completed Activity 4

---
+++ activity-4/js/scripts.js
@@ -0,0 +1,17 @@
+//Welcome Message!
+var user = 'Pedro';
+var salutation = 'Hello, ';
+var greeting = salutation + user + '! Here are the latest Atom reviews.';
+var greetingEl = document.getElementById('greeting');
+
+greetingEl.textContent = greeting;
+
+// Product Price
+var price = 20,
+    studentDiscount = .10,
+studentPrice = price - (price * studentDiscount),
+    priceEl = document.getElementById('price'),
+    studentPriceEl = document.getElementById('student-price');
+
+priceEl.textContent = price.toFixed(2);
+studentPriceEl.textContent = studentPrice.toFixed(2);
